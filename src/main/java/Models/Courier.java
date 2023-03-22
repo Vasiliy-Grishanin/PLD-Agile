a720d0016ee86b24f63032fac862a0c9fbb30f42
vasiliy-grishanin <vasiliy.grishanin@etu.unice.fr>
2023-03-22 16:55:59 +0100

    Modèle Courier

--- src/main/java/Models/Courier.java
+++ src/main/java/Models/Courier.java
@@ -1,4 +1,21 @@
 package Models;
 
+import java.util.ArrayList;
+
 public class Courier {
+    private static int nextId = 1;
+    private final int id;
+    private ArrayList<Delivery> deliveries = new ArrayList<Delivery>();
+
+    public Courier () {
+        id = nextId;
+        nextId++;
+    }
+
+    public Courier (int courierId) {
+        this.id = courierId;
+        if (courierId >= nextId) {
+            nextId = courierId + 1;
+        }
+    }
 }
